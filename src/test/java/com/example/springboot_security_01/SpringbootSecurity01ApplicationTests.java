cb2c6a9d2e7173aa24f843c99864af1b2f44c6ca
ycc <874552640@qq.com>
2022-01-24 15:30:04 +0800

    新建了一个hellocontroller

---
+++ src/test/java/com/example/springboot_security_01/SpringbootSecurity01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_security_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSecurity01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
